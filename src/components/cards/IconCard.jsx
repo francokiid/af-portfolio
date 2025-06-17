9c01a4e761567802441345aafeccc7c05405b93c
Top Drive <v.angelfranco@gmail.com>
2025-06-17 16:13:40 +0800

    Revised

--- src/components/cards/IconCard.jsx
+++ src/components/cards/IconCard.jsx
@@ -1,6 +1,6 @@
 export const IconCard = ({icon, label}) => {
   return (
-    <div className="w-20 h-20 md:w-28 md:h-28 flex flex-col items-center justify-center bg-neutral-800 rounded-md md:rounded-lg p-2 gap-2 md:gap-4">
+    <div className="w-18 h-18 md:w-28 md:h-28 flex flex-col items-center justify-center bg-neutral-800 rounded-md md:rounded-lg p-2 gap-2 md:gap-4">
       <div className="text-neutral-200 text-2xl md:text-4xl">{icon}</div>
       <span className="text-neutral-200 text-xs md:text-sm">{label}</span>
     </div>
